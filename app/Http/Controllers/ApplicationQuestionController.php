382945d74ee15fcd5bd5ba5ce9118291058cafc1
PadBro <padmaster2012@googlemail.com>
2024-12-31 01:48:06 +0100

    feat: adjust application for bot

--- app/Http/Controllers/ApplicationQuestionController.php
+++ app/Http/Controllers/ApplicationQuestionController.php
@@ -21,8 +21,10 @@ class ApplicationQuestionController extends Controller
             abort(403);
         }
         $applicationQuestion = QueryBuilder::for(ApplicationQuestion::class)
+            ->defaultSort('order')
+            ->allowedSorts('order')
             ->allowedFilters([
-                'question',
+                AllowedFilter::exact('is_active'),
                 AllowedFilter::exact('id'),
             ])
             ->getOrPaginate();
